c81f3d9074453b9c4f08a118895cbf4dd642ad67
william <william.lee@gtedx.com>
2017-04-05 09:37:36 +0800

    Object

--- Js01/js04.js
+++ Js01/js04.js
@@ -33,8 +33,10 @@ arr; // arr变为[1, 2, 3, undefined, undefined, 'x']
 // 大多数其他编程语言不允许直接改变数组的大小，越界访问索引会报错。然而，JavaScript的Array却不会有任何错误。
 // 在编写代码时，不建议直接修改Array的大小，访问索引时要确保索引不会越界。
 
-// indexOf
-// 与String类似，Array也可以通过indexOf()来搜索一个指定的元素的位置：
+/**
+ *indexOf
+ * 与String类似，Array也可以通过indexOf()来搜索一个指定的元素的位置：
+ */
 
 var arr = [10, 20, '30', 'xyz'];
 arr.indexOf(10); // 元素10的索引为0
@@ -43,8 +45,10 @@ arr.indexOf(30); // 元素30没有找到，返回-1
 arr.indexOf('30'); // 元素'30'的索引为2
 // 注意了，数字30和字符串'30'是不同的元素。
 
-// slice
-// slice()就是对应String的substring()版本，它截取Array的部分元素，然后返回一个新的Array：
+/**
+ *slice
+ * slice()就是对应String的substring()版本，它截取Array的部分元素，然后返回一个新的Array：
+ */
 
 var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 arr.slice(0, 3); // 从索引0开始，到索引3结束，但不包括索引3: ['A', 'B', 'C']
@@ -58,49 +62,63 @@ var aCopy = arr.slice();
 aCopy; // ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 aCopy === arr; // false
 
-// push和pop
-// push()向Array的末尾添加若干元素，pop()则把Array的最后一个元素删除掉：
+/**
+push和pop
+push()向Array的末尾添加若干元素，pop()则把Array的最后一个元素删除掉：
+*/
 
 var arr = [1, 2];
 arr.push('A', 'B'); // 返回Array新的长度: 4
 arr; // [1, 2, 'A', 'B']
 arr.pop(); // pop()返回'B'
 arr; // [1, 2, 'A']
-arr.pop(); arr.pop(); arr.pop(); // 连续pop 3次
+arr.pop();
+arr.pop();
+arr.pop(); // 连续pop 3次
 arr; // []
 arr.pop(); // 空数组继续pop不会报错，而是返回undefined
 arr; // []
 
-// unshift和shift
-// 如果要往Array的头部添加若干元素，使用unshift()方法，shift()方法则把Array的第一个元素删掉：
+/**
+unshift和shift
+如果要往Array的头部添加若干元素，使用unshift()方法，shift()方法则把Array的第一个元素删掉：
+*/
 
 var arr = [1, 2];
 arr.unshift('A', 'B'); // 返回Array新的长度: 4
 arr; // ['A', 'B', 1, 2]
 arr.shift(); // 'A'
 arr; // ['B', 1, 2]
-arr.shift(); arr.shift(); arr.shift(); // 连续shift 3次
+arr.shift();
+arr.shift();
+arr.shift(); // 连续shift 3次
 arr; // []
 arr.shift(); // 空数组继续shift不会报错，而是返回undefined
 arr; // []
 
-// sort
-// sort()可以对当前Array进行排序，它会直接修改当前Array的元素位置，直接调用时，按照默认顺序排序：
+/**
+sort
+ sort()可以对当前Array进行排序，它会直接修改当前Array的元素位置，直接调用时，按照默认顺序排序：
+*/
 
 var arr = ['B', 'C', 'A'];
 arr.sort();
 arr; // ['A', 'B', 'C']
 // 能否按照我们自己指定的顺序排序呢？完全可以，我们将在后面的函数中讲到。
 
-// reverse
-// reverse()把整个Array的元素给掉个个，也就是反转：
+/**
+reverse
+reverse()把整个Array的元素给掉个个，也就是反转：
+ */
 
 var arr = ['one', 'two', 'three'];
 arr.reverse();
 arr; // ['three', 'two', 'one']
 
-// splice
-// splice()方法是修改Array的“万能方法”，它可以从指定的索引开始删除若干元素，然后再从该位置添加若干元素：
+/**
+splice
+splice()方法是修改Array的“万能方法”，它可以从指定的索引开始删除若干元素，然后再从该位置添加若干元素：
+*/
 
 var arr = ['Microsoft', 'Apple', 'Yahoo', 'AOL', 'Excite', 'Oracle'];
 // 从索引2开始删除3个元素,然后再添加两个元素:
@@ -113,8 +131,10 @@ arr; // ['Microsoft', 'Apple', 'Oracle']
 arr.splice(2, 0, 'Google', 'Facebook'); // 返回[],因为没有删除任何元素
 arr; // ['Microsoft', 'Apple', 'Google', 'Facebook', 'Oracle']
 
-// concat
-// concat()方法把当前的Array和另一个Array连接起来，并返回一个新的Array：
+/**
+concat
+concat()方法把当前的Array和另一个Array连接起来，并返回一个新的Array：
+*/
 
 var arr = ['A', 'B', 'C'];
 var added = arr.concat([1, 2, 3]);
@@ -127,15 +147,19 @@ arr; // ['A', 'B', 'C']
 var arr = ['A', 'B', 'C'];
 arr.concat(1, 2, [3, 4]); // ['A', 'B', 'C', 1, 2, 3, 4]
 
-// join
-// join()方法是一个非常实用的方法，它把当前Array的每个元素都用指定的字符串连接起来，然后返回连接后的字符串：
+/**
+join
+join()方法是一个非常实用的方法，它把当前Array的每个元素都用指定的字符串连接起来，然后返回连接后的字符串：
+*/
 
 var arr = ['A', 'B', 'C', 1, 2, 3];
 arr.join('-'); // 'A-B-C-1-2-3'
 // 如果Array的元素不是字符串，将自动转换为字符串后再连接。
 
-// 多维数组
-// 如果数组的某个元素又是一个Array，则可以形成多维数组，例如：
+/**
+多维数组
+如果数组的某个元素又是一个Array，则可以形成多维数组，例如：
+*/
 
 var arr = [[1, 2, 3], [400, 500, 600], '-'];
 // 上述Array包含3个元素，其中头两个元素本身也是Array。
